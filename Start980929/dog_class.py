62d44131c32fd8a46ad7b6ee81399507b8a07c27
Alireza Abbasi <AlirezaAbbasiM@gmail.com>
2020-02-07 18:23:25 +0330

    create Dog class and test it

---
+++ Start980929/dog_class.py
@@ -0,0 +1,18 @@
+
+class dog():
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name
+
+
+    def sit(self):
+        pass
+
+
+    def roll_over(self):
+        pass
+
+first_sample = dog(2, "Jack")
+
+print("dog name is: ",first_sample.name)
+print("It's age is: ", first_sample.age)
